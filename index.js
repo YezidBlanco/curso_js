2df4936468027fe7ac0f0ad9aa752a18b56ca914
YezidBlanco <yezidblanco@gmail.com>
2024-10-23 19:55:10 -0500

    Se modifica el archivo index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log('Hola mundo, mi nombre es: Yezid Blanco')
+console.log('Entrada: Introduces tu tarjeta y selecciona la cantidad de dinero que deseas retirar')
+console.log('Proceso: El cajero verifica tu información y realiza la transacción en tu cuenta bancaria')
+console.log('Salida: Recibes el dinero solicitado y un recibo')
